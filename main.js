f58d088b8c2afc91bd2c42fd8b047052994035db
Tashvi232 <79177478+Tashvi232@users.noreply.github.com>
2021-04-09 12:02:29 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -15,4 +15,74 @@ function player_update() {
          }); 
          canvas.add(player_object);
       });
-      } 
+      }
+
+      function new_image(get_image) {
+        fabric.Image.fromURL(get_image, function(Img) {
+             block_image_object = Img; block_image_object.scaleToWidth(block_image_width); 
+             block_image_object.scaleToHeight(block_image_height);
+              block_image_object.set({ top:player_y, left:player_x });
+               canvas.add(block_image_object); 
+              });
+               }
+      window.addEventListener("keydown",my_keydown);
+      
+      function my_keydown(e) {
+        keyPressed=e.keyCode;
+        console.log(keyPreseed);
+        if(e.shift==true && keyPressed=="80") {
+            block_image_width=block_image_width+10;
+            block_image_height=block_image_height+10;
+            document.getElementById("current_width").innerHTML=block_image_width;
+            document.getElementById("current_height").innerHTML=block_image_height;
+        }
+        if(e.shift==true && keyPressed=="77") {
+            block_image_width=block_image_width-10;
+            block_image_height=block_image_height-10;
+            document.getElementById("current_width").innerHTML=block_image_width;
+            document.getElementById("current_height").innerHTML=block_image_height;
+        }
+        if(keyPressed=="70"){
+            new_image('thor_face.png');
+            console.log("f");
+        }
+        
+        if(keyPressed=="66"){
+            new_image('ironman_body.png');
+            console.log("b");
+        }
+
+        if(keyPressed=="76"){
+            new_image('spiderman_legs.png');
+            console.log("l");
+        }
+
+        if(keyPressed=="82"){
+            new_image('hulk_right_hand.png');
+            console.log("r");
+        }
+
+        if(keyPressed=="72"){
+            new_image('captain_america_left_hand.png');
+            console.log("h");
+        }
+
+        if(keyPressed=="38"){
+            up();
+            console.log("up");
+        }
+        if(keyPressed=="40") {
+            down();
+            console.log("down");
+    
+        }
+    
+        if(keyPressed=="37"){
+            left();
+            console.log("left");
+        }
+        if(keyPressed=="39"){
+           right();
+           console.log("right");
+        }
+    }
